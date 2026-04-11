b2784f0ba8915f9194735b82b710af7000834250
황상윤 <s_infinite@naver.com>
2026-04-11 22:10:53 +0900

    [init] next 설치

---
+++ next.config.ts
@@ -0,0 +1,9 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/sorting-visualizer',
+  assetPrefix: '/sorting-visualizer/',
+};
+
+export default nextConfig;
