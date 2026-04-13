a75d2b2f55b7a81463cda247b90b3fdf6eaca78c
sangyun-hwang <sangyun-hwang@users.noreply.github.com>
2026-04-14 00:08:26 +0900

    [feat] git hub deploy 파일 수정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,7 @@ const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig: NextConfig = {
   output: "export",
+  images: { unoptimized: true },
   basePath: isProd ? "/sorting-visualizer" : "",
   assetPrefix: isProd ? "/sorting-visualizer" : "",
 };
