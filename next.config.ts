828afbd9597f7749b2ab41f87d031ae939bbc250
sangyun-hwang <sangyun-hwang@users.noreply.github.com>
2026-04-14 00:23:16 +0900

    [feat] next config 수정

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,11 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === "production"
 
 const nextConfig: NextConfig = {
   output: "export",
   images: { unoptimized: true },
-  basePath: isProd ? "/sorting-visualizer/" : "",
-  assetPrefix: isProd ? "/sorting-visualizer/" : "",
+  basePath: "/sorting-visualizer/",
+  assetPrefix: "/sorting-visualizer/",
 };
 
 export default nextConfig;
